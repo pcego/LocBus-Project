26f99c5aa8415b19d7de5f610bbccc2ca476effb
Kennedi Paulo <kennedimalheiros@gmail.com>
2013-07-23 21:13:16 -0300

    Criando métodos do DAO Genérico.

--- locBus/src/java/br/com/kpc/locbus/Dao/DaoGenerico.java
+++ locBus/src/java/br/com/kpc/locbus/Dao/DaoGenerico.java
@@ -5,28 +5,66 @@
 package br.com.kpc.locbus.Dao;
 
 import br.com.kpc.locbus.core.IRepositorio;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
  * @author César
  */
-public class DaoGenerico implements IRepositorio {
+public abstract class DaoGenerico<T> implements IRepositorio<T> {
+
+    private Class type;
+    @PersistenceContext(unitName = "locBusPU")
+    protected EntityManager manager;
+
+    protected EntityManager getManager() {
+        return manager;
+    }
+
+    public DaoGenerico(Class tipo) {
+        type = tipo;
+    }
+
+    protected abstract Long getCodigo(T obj);
 
     @Override
     public boolean salvar(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            getManager().persist(obj);
+            getManager().flush();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean apagar(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //     throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            getManager().remove(getManager().getReference(type, obj));
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean atualizar(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            getManager().refresh(obj);
+            getManager().flush();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
-    
     
     
 }
