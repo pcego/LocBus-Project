22e27e89b4b985a9513d780a2d6719dc41b87942
pcego <pcego36@gmail.com>
2013-08-19 20:47:34 -0300

    criando facade web service para posicao

--- locBus/src/java/br/com/kpc/locbus/resource/PosicaoResource.java
+++ locBus/src/java/br/com/kpc/locbus/resource/PosicaoResource.java
@@ -4,6 +4,9 @@
  */
 package br.com.kpc.locbus.resource;
 
+import br.com.kpc.locbus.core.Posicao;
+import br.com.kpc.locbus.core.Veiculo;
+import java.util.List;
 import javax.ws.rs.Path;
 
 /**
@@ -11,8 +14,20 @@ import javax.ws.rs.Path;
  * @author César
  */
 @Path("/posicoes")
-public class PosicaoResource extends PosicaoFacade{
-        
-    
-    
+public class PosicaoResource extends PosicaoFacade {
+
+    @Override
+    public Long getCodigo(Posicao obj) {
+        return super.getCodigo(obj);
+    }
+
+    @Override
+    public Posicao getUltimaPosicao(Veiculo veiculo) {
+        return super.getUltimaPosicao(veiculo);
+    }
+
+    @Override
+    public List<Posicao> getPosiçoesPorVeiculo(Veiculo veiculo) {
+        return super.getPosiçoesPorVeiculo(veiculo);
+    }
 }
